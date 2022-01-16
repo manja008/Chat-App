ad58b06d92be4b49b5870aaf1a698df66b3a7716
Manja Verber <manja.verber@gmail.com>
2022-01-16 15:46:04 +0100

    react chat app structure

---
+++ src/Components/Input.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+
+
+export default function Input({onSendMessage}) {
+const [text, setText] = useState('');
+
+const changeText =(e)=>{
+    const chatText = e.target.value;
+    setText(chatText.trimStart());
+}
+
+const handleSubmit =(e)=>{
+    e.preventDefault(); // must as we have no server to send
+    onSendMessage(text);
+    setText('');
+}
+
+return (
+    <div>
+        <form onSubmit={handleSubmit} className="input">
+            <input 
+            className='inputChat'
+            onChange={changeText}
+            value={text}
+            type='text'
+            placeholder='"Lets chat"'
+            autoFocus={true}
+            ></input>
+            <button type="submit" className="inputBtn" onSubmit={handleSubmit}>
+                <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+        </form>
+    </div>
+);
+}
